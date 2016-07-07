aa28b5eb233e0343a135240e675d5d89387a1ea4
akribopo <akribopo@gmail.com>
2016-07-07 20:52:03 +0300

    initial commit

---
+++ user/.output/eagle/debug/obj/user_main.d
@@ -0,0 +1,22 @@
+.output/eagle/debug/obj/user_main.o .output/eagle/debug/obj/user_main.d : user_main.c \
+ /home/parallels/ESP8266_NONOS_SDK/include/ets_sys.h \
+ /home/parallels/ESP8266_NONOS_SDK/include/c_types.h \
+ /home/parallels/ESP8266_NONOS_SDK/include/eagle_soc.h \
+ /home/parallels/ESP8266_NONOS_SDK/include/osapi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /opt/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../include/user_config.h ../include/spi_test.h
